eab85a7bc8632b248df2a1fc3bd5f831b47c5ad2
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2022-01-15 19:59:41 -0500

    fixed typo in production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://safe-earth-88969.herokuapp.com/',
+  production: 'https://safe-earth-88969.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
